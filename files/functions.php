dc6599453391379c735e9794c5e66717e35495d7
upender-gleecus <39897780+upender-gleecus@users.noreply.github.com>
2019-11-07 16:30:26 +0530

    Add files via upload

---
+++ files/functions.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @copyright  Copyright (c) (http://www.winterinfotech.com)
+ * @license    http://www.winterinfotech.com/license/
+ * @author         Webibazaar
+ * @version        Release: 1.0
+ */
+
+if ( ! function_exists( 'wntr_child_scripts' ) ) :
+function wntr_child_scripts() {
+    wp_enqueue_style( 'megamall-child-style', get_template_directory_uri(). '/style.css');	
+}
+endif;
+add_action( 'wp_enqueue_scripts', 'wntr_child_scripts' );
+
+if ( ! function_exists( 'wntr_child_resscripts' ) ) :
+function wntr_child_resscripts() {
+    wp_enqueue_style( 'megamall-child-resstyle', get_template_directory_uri(). '/responsive.css');	
+}
+endif;
+add_action( 'wp_enqueue_scripts', 'wntr_child_resscripts' );
+add_filter( 'woocommerce_ship_to_different_address_checked', '__return_false' );
